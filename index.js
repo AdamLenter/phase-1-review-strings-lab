e69b39f6c49b79a4b591fe9e1e6e23f9227e7f85
Adam <adamlenter1@gmail.com>
2021-09-21 12:28:54 -0400

    commit tree e2908d9f2a596e7fa4347d890f0f4ce0fafb1da5

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Bob Sacamano";
+const welcomeMessage = `Welcome to Flatbook, ` + currentUser + `!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.substring(0,1)}!`;
+console.log(shortGreeting);
